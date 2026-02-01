143c9d0a450f35d5797775e927647b353e13b0eb
Emiliano Chiarini <emiliano40017@gmail.com>
2026-02-01 12:46:48 +0100

    cambiato nome ai circuiti, resi standard

--- src/TrackEnv.py
+++ src/TrackEnv.py
@@ -139,7 +139,7 @@ class TrackEnv(gym.Env):
         self.metadata = {"render_fps": 10, "render_modes": ["human", "rgb_array"]}
         
         self.matrix = buildTrack()
-        self.distance_matrix = creaMatriceDistanze("./imola_track.csv", "destra")
+        self.distance_matrix = creaMatriceDistanze("./track_imola.csv", "destra")
         
         # --- VARIABILI PER IL RENDERING ---
         self.render_mode = render_mode
@@ -467,7 +467,7 @@ class TrackEnv(gym.Env):
 
 
 
-def buildTrack(fileName = "./imola_track.csv"):
+def buildTrack(fileName = "./track_imola.csv"):
   df = pd.read_csv(fileName, header = None
                    #,sep=';'
                    )
